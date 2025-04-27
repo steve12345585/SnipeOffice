5fdd17430de655d521271aadca06a4291c9570d8
steve12345585 <steve12345585@github.com>
2025-04-27 18:04:35 +0200

    Update UTF-8 BOM handling
    Change-Id: I7992dcf00477f5c915b36bc2786e73daf6e98b02

--- sc/source/ui/docshell/docsh.cxx
+++ sc/source/ui/docshell/docsh.cxx
@@ -1,6 +1,6 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- * This file is Part of the SnipeOffice project.
+ * This file is Part of the SnipeOffice project. MASJOR BOM
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
